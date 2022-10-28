b9cd39c85e28be124b1ae34a21011aab4d5caf2e
mohammad <114822441+mohammad77h@users.noreply.github.com>
2022-10-28 16:18:06 -0700

    .

---
+++ day 6/day 6/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  day 6
+//
+//  Created by Mac on 26/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var weight = ""
+    @State var height = ""
+    
+    @State var BMI = ""
+    @State var Status = ""
+    
+    var body: some View {
+        VStack {
+            
+            Image("weight")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 130, height: 135)
+                .padding()
+            
+            Text("ادخل الطول و الوزن")
+                .font(.callout)
+                .fontWeight(.semibold)
+            
+            
+            
+            TextField("enter your weight in KG", text: $weight)
+            
+            TextField("enter your height in meters", text: $height)
+            
+            Button(action: {BMICalulator(heigh: Double(height) ?? 1.0, weight: Double(weight) ?? 1.0)}, label: {
+                Text("احسب")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                
+            })
+            
+        }
+        
+        .frame(width: 500, height: 280)
+        .background(Color.orange)
+        .contrast(10)
+        
+        Text("BMI = \(BMI)")
+        Text("Status = \(Status)")
+    }
+    func BMICalulator(heigh: Double, weight: Double){
+        var mohammed = weight / (heigh * heigh)
+        if mohammed>=20 {
+            Status = "ضعيف"
+        }else if mohammed <= 25 {
+            Status = "جيد"
+        } else {
+            Status = "سمين"
+        }
+          BMI = "\(mohammed)"
+            
+            
+            
+            
+            
+            
+            struct ContentView_Previews: PreviewProvider {
+                static var previews: some View {
+                    ContentView()
+                }
+            }
+        }
+        
+        
+        
+        
+        
+    }
+
---
+++ day 6/day 6/day_6App.swift
@@ -0,0 +1,17 @@
+//
+//  day_6App.swift
+//  day 6
+//
+//  Created by Mac on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct day_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
